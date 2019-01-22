985408db925b9d64664e3e684b69b23a23be44c6
Adian Tsosie <addve12@yahoo.com>
2019-01-22 08:45:29 -0700

    Made the for statement.

---
+++ snap-loops.js
@@ -0,0 +1,12 @@
+{//					 0  1  2  3  5  8  13  21  34
+	let ourArray = [0, 1, 2, 3, 5, 8, 13, 21, 34];
+
+	let arrLen = ourArray.length;
+	for(let i = 0; i<arrLen; i++){
+//	console.log("i is equal to: " + i)
+		console.log(ourArray[i]);
+		for(let j = 0; j<10; j++){
+			console.log('j is equal to: ' + j);
+		}
+	}
+}
